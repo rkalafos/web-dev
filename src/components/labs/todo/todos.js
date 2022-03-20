7363ac2c34fe5eab4c9a9dd7e450060888bbe091
Rachel Kalafos <kalafos.r@northeastern.edu>
2022-03-20 19:38:01 -0400

    todo list variable assign fix

--- src/components/labs/todo/todos.js
+++ src/components/labs/todo/todos.js
@@ -1,5 +1,6 @@
-export default [
+const todo_list = [
     { "title": "Buy milk",               "status": "CANCELED",       "done": true    },
     { "title": "Pickup the kids",   "status": "IN PROGRESS",  "done": false  },
     { "title": "Walk the dog",       "status": "DEFERRED",        "done": false  },
 ];
+export default todo_list;
